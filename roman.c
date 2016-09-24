1e20202b9728a27ed10c854ce832b5f03a307f4f
lucascamposj <lucascj29@gmail.com>
2016-09-24 13:12:57 -0300

    Desenvolvimento e teste da função RomanoValido

--- roman.c
+++ roman.c
@@ -59,3 +59,62 @@ result ArabCorrespondente(const char *rom_char,int *rom_int,int tam)
 	/*Caso não tenho ocorrido caracteres inválidos. A função retorna OK. */
 	return OK;
 }
+
+/**
+* @brief Testa erros de sintaxe do algorismo romano inserido.
+*/
+/**@param rom_int Vetor de números inteiros correspondentes ao algarismos romanos.
+ * @param tam_s Quantidade de algarismos, tamanho da string inserida.
+ */
+/**@return Ok, caso o algarismo romano inserido seja válido.
+ * @return ERRO, caso contrário.
+ */ 
+
+result RomanoValido(int *rom_int,int tam_s)
+{
+	int i,j;
+	boolean flag; /* Variável que verifica se controla a ocorrência de algarismos repitidos*/
+	
+	/* Verifica casos onde a mais de 3 algarismos iguais seguidos
+	 */
+	/* Exemplo de invalidez: VIIII
+	 */
+	for(i = 0, flag = TRUE; i <= (tam_s-4) && flag == TRUE; i++){
+		flag = FALSE;
+		for (j = i; j < i+4; j++)
+			if(rom_int[i] != rom_int[j])
+				flag = TRUE;
+		if(flag == FALSE)
+			return ERRO;
+	}
+
+	/* 	Verifica casos onde a partir do segundo algarismo anterior é menor.
+	 */
+	/*	Exemplo de invalidez: IIV
+	 */
+	for (i = 2; i < tam_s; i++){
+		for(j = 2; (i-j) >= 0; j++){
+			if(rom_int[i - j] < rom_int[i]) return ERRO;
+		}
+	}
+
+
+	/*Verifica de casos onde ocorre subtração de algarismos inválidos.*/
+	for (i = 1; i < tam_s; i++){
+		/* Verifica de algarismos especiais V,L,D.
+		 * Tais algarismos não podem aparecer antes de algarismos maiores.
+		 */
+		/* Exemplo de invalidez: VX.
+		 */
+		if(rom_int[i - 1] < rom_int[i] && (rom_int[i-1] == 5 || rom_int[i-1] == 50 || rom_int[i-1] == 500))
+			return ERRO;
+		/*	Verifica casos onde há subtração, que só pode ocorrer caso esteja subtraindo um décimo ou um quinto do valor verificado.
+		 */
+		/*	Exemplos de invalidez: IC, XM.
+		 */
+		if(rom_int[i-1] < rom_int[i] && rom_int[i-1] != (rom_int[i]/10) &&  rom_int[i-1] != (rom_int[i]/5))
+			return ERRO;
+	}	
+	/*Caso não tenham ocorridos erros de formatação do número romano. A função retorna OK.*/
+	return OK;
+}
--- teste.c
+++ teste.c
@@ -18,6 +18,92 @@ TEST (ArabCorrespondente, CaracteresValidos)
 	EXPECT_EQ (ERRO, ArabCorrespondente("MDCLJXVI", vetor, 8));
 }
 
+/**@brief Testa a condição sobre Algarismo Seguidos da função RomanoValido.*/
+/**Verifica se o retorno é OK caso haja mais de 3 algarismos romanos iguais seguidos.
+ * Caso contrário verifica se o retorno é igual a ERRO.
+ */
+
+TEST (RomanoValido, AlgarismosSeguidos)
+{
+	int vetor[30] = {10,1,1,1};
+
+	EXPECT_EQ (OK, RomanoValido(vetor, 4));
+
+	vetor[0] = 10;
+	vetor[1] = 1;
+	vetor[2] = 1;
+	vetor[3] = 1;
+	vetor[4] = 1;	
+
+	EXPECT_EQ (ERRO, RomanoValido(vetor, 5));
+}
+
+/**@brief Testa a condição sobre Algarismo Anteriores da função RomanoValido.*/
+/**Verifica se o retorno é OK, caso para cada algarismo, haja um algarismo anterior de maior valor,
+ * desconsiderando o vizinho anterior.
+ * Caso contrário verifica se o retorno é igual a ERRO.
+ */
+
+TEST (RomanoValido, AlgarismosAnteriores)
+{
+	int vetor[30] = {50,1,10};
+	
+	EXPECT_EQ (OK, RomanoValido(vetor, 3));
+
+	vetor[0] = 10;
+	vetor[1] = 1;
+	vetor[2] = 1;
+	vetor[3] = 5;
+	vetor[4] = 100;
+	EXPECT_EQ (ERRO, RomanoValido(vetor, 5));
+}
+
+/**@brief Testa a condição sobre Algarismo Especiais da função RomanoValido.*/
+/**Verifica se o retorno é ERRO caso o haja um algorismo seguinte de V,L, ou D, maior.
+ * Caso contrário verifica se o retorno é igual a OK.
+ */
+
+TEST (RomanoValido, AlgarismosEspeciais)
+{
+	int vetor[30] = {5,10,1};
+	
+	EXPECT_EQ (ERRO, RomanoValido(vetor, 3));
+
+	vetor[0] = 1000;
+	vetor[1] = 500;
+	vetor[2] = 1000;
+	EXPECT_EQ (ERRO, RomanoValido(vetor, 3));
+
+	vetor[0] = 50;
+	vetor[1] = 100;
+	EXPECT_EQ (ERRO, RomanoValido(vetor, 2));
+
+	vetor[0] = 500;
+	vetor[1] = 100;
+	EXPECT_EQ (OK, RomanoValido(vetor, 2));
+}
+
+/**@brief Testa a condição sobre a sintaxe, envolvendo subtração, de números romanos.*/
+/**Verifica se o retorno é ERRO caso haja um número seguinte maior, diferente de 10 ou 5 vezes o seu valor.
+ * Caso contrário verifica se o retorno é igual a OK.
+ */
+
+TEST (RomanoValido, Subtracao)
+{
+	int vetor[30] = {1,100};
+	
+	EXPECT_EQ (ERRO, RomanoValido(vetor, 3));
+
+	vetor[0] = 1000;
+	vetor[1] = 10;
+	vetor[2] = 1000;
+	EXPECT_EQ (ERRO, RomanoValido(vetor, 3));
+
+	vetor[0] = 10;
+	vetor[1] = 50;
+	EXPECT_EQ (OK, RomanoValido(vetor, 2));
+}
+
 int main(int argc, char **argv)
 {
 	::testing::InitGoogleTest(&argc, argv);
